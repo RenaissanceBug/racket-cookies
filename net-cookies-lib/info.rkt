4053e422af79d5ab5d57a6c9b758b6311495fc4a
Bogdan Popa <bogdan@defn.io>
2023-12-27 19:01:22 +0200

    build: drop srfi-lite-lib dependency

--- net-cookies-lib/info.rkt
+++ net-cookies-lib/info.rkt
@@ -2,6 +2,6 @@
 
 (define collection 'multi)
 (define pkg-desc "implementation (no documentation) part of \"net-cookies\"")
-(define deps '("srfi-lite-lib" "base"))
+(define deps '("base"))
 (define version "1.1.4")
 (define license '(Apache-2.0 OR MIT))
