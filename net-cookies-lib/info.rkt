a1f52da1f9e55a6095c77e20d858661573a60946
Bogdan Popa <bogdan@defn.io>
2023-12-27 19:31:21 +0200

    build: bump version number

--- net-cookies-lib/info.rkt
+++ net-cookies-lib/info.rkt
@@ -3,5 +3,5 @@
 (define collection 'multi)
 (define pkg-desc "implementation (no documentation) part of \"net-cookies\"")
 (define deps '("base"))
-(define version "1.1.4")
+(define version "1.2")
 (define license '(Apache-2.0 OR MIT))
